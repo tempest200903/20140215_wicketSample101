fd9b05a13194647e36c1531f79a103a49a73c220
tempest200903 <tempest200903@gmail.com>
2014-02-15 20:56:05 +0900

    時刻を表示する http://gihyo.jp/dev/feature/01/wicket/0002?page=3

--- myproject/src/main/java/com/mycompany/Addressbook.java
+++ myproject/src/main/java/com/mycompany/Addressbook.java
@@ -1,8 +1,12 @@
 package com.mycompany;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 
 public class Addressbook extends WebPage {
 	private static final long serialVersionUID = 1L;
@@ -10,8 +14,16 @@ public class Addressbook extends WebPage {
 	public Addressbook(final PageParameters parameters) {
 		super(parameters);
 
-		add(new Label("message",
-				"If you see this message wicket is properly configured and running"));
+		if (false) {
+			add(new Label("message",
+					"If you see this message wicket is properly configured and running"));
+
+		}
+		add(new Label("message", new AbstractReadOnlyModel<String>() {
+			public String getObject() {
+				return new SimpleDateFormat("HH時mm分ss秒").format(new Date());
+			}
+		}));
 		add(new Label("version", getApplication().getFrameworkSettings()
 				.getVersion()));
 	}
